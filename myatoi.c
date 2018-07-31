a53a080d6cd528d15489b470aabd75f9295f7900
Rafael <raf07ray@gmail.com>
2018-07-31 11:42:00 -0400

    updated to work with negative numbers

--- myatoi.c
+++ myatoi.c
@@ -28,18 +28,25 @@ int myatoi(const char *str){
   
   unsigned size = get_str_size(str);
   int number = 0;
-  for(int i = size-2;i>=0;i--){
+  int end = 0;
+  int sign = 1;
+  // detect negatives
+  if(str[0] == 45){
+    end = 1;
+    sign = -1;
+  }
+  for(int i = end;i<size-1;i++){
     //make sure the value is a number
     assert(str[i] <= 57);
     assert(str[i] >= 48);
 
-    number += (str[i]-48)*ten_raised(i);
+    number += sign*(str[i]-48)*ten_raised(size-2-i);
    }
   return number;
 }
 
 int main(){
-  const char name[] = "1234";
+  const char name[] = "237843";
   
   int number = myatoi(name);
   printf("value given: %s.\n",name);

801dd734e5d728d5b2be53aa85ed73cb501a4bc2
Rafael <raf07ray@gmail.com>
2018-07-31 14:48:49 -0400

    working version

---
+++ myatoiHex.h
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#ifndef MYATOIHEX_H_
+#define MYATOIHEX_H_
+
+int myatoiHex(const char *str);
+
+int sixteen_raised(int pow);
+
+#endif

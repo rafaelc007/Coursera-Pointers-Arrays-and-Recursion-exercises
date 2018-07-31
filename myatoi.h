848b636122e1e9e55dcdf989bafe5cd02587cdb3
Rafael <raf07ray@gmail.com>
2018-07-31 13:36:55 -0400

    structured terms

---
+++ myatoi.h
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#ifndef MYATOI_H_
+#define MYATOI_H_
+
+int ten_raised(int pow);
+
+unsigned get_str_size(const char *str);
+
+int myatoi(const char *str);
+
+#endif

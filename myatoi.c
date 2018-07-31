8c9e37aa918e260640829e91a20219b1a59b2d6a
Rafael <raf07ray@gmail.com>
2018-07-31 11:13:20 -0400

    tryed to fix inversion bug

--- myatoi.c
+++ myatoi.c
@@ -11,24 +11,30 @@ int ten_raised(int pow){
   return value;
 }
 
+unsigned get_str_size(const char *str){
+  unsigned size = 0;
+  while(1){
+    if(str[size] == '\0'){
+      return size+1;
+    }
+    else{
+      size++;
+  }
+}
+
 int myatoi(const char *str){
   // function that converts a numeric string into a integer number
   
-  int i = 0;
+  unsigned size = get_str_size(str);
   int number = 0;
-  while(1){
-    if(str[i] == '\0'){
-      return number;
-    }
-   
+  for(unsigned i = size-1;i>=0;i--){
     //make sure the value is a number
     assert(str[i] <= 57);
     assert(str[i] >= 48);
 
     number += (str[i]-48)*ten_raised(i);
-    i++;
    }
-  return -1;
+  return number;
 }
 
 int main(){
